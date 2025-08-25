315a9bac44f1e6a013ff97b763a3956199ba8284
DiandreRuiz <Diandreruiz77@gmail.com>
2025-08-25 14:46:57 -0400

    Finished implementing random cat photo API

--- app.js
+++ app.js
@@ -6,31 +6,47 @@
 // Append <img> element as new child to api0-container div
 
 // API Endpoints
-DOG_API = "https://dog.ceo/api/breeds/image/random";
+DOG_IMG_API = "https://dog.ceo/api/breeds/image/random";
+CAT_IMG_API = "https://api.thecatapi.com/v1/images/search";
 
 // Initialize variables for all API Card buttons
-const api0Button = document.querySelector("#api-0-button");
+const dogAPIButton = document.querySelector("#api-0-button");
+const catAPIButton = document.querySelector("#api-1-button");
 
 const singleAPICall = async (endpointURL) => {
     const response = await fetch(endpointURL); // Make call to api & await response
-    const data = await response.json(); // Ask browser to parse readable stream into JSON object
-    return data.message;
+    return response;
 };
 
-// Button Listeners for API Calls
-api0Button.addEventListener("click", async () => {
-    const dogPhotoURL = await singleAPICall(DOG_API);
-    const dogPhotoContainer = document.querySelector("#api0-container");
-    const dogIMG = document.createElement("img");
+// Replace <img> in API card
+const replaceIMG = (newImgUrl, containerID, ImgPlaceholder) => {
+    // Create new <img>
+    const newImg = document.createElement("img");
+    newImg.src = newImgUrl;
+    newImg.placeholder = ImgPlaceholder;
+    newImg.style.maxWidth = "100%";
 
-    // Clear previous dog image if it exists
-    dogPhotoContainer.innerHTML = "";
+    // Clear old <img> if there is one
+    const imgContainer = document.querySelector(containerID);
+    imgContainer.innerHTML = "";
 
-    // Create new img element with returned URL
-    dogIMG.src = dogPhotoURL;
-    dogIMG.classList.add("img-fluid", "rounded", "mx-auto", "d-block");
-    dogIMG.alt = "A Random Dog";
+    // Append new <img> to container
+    imgContainer.appendChild(newImg);
+};
 
-    // Append new img element
-    dogPhotoContainer.appendChild(dogIMG);
+// Random Image Generators (0 & 1)
+dogAPIButton.addEventListener("click", async () => {
+    const dogPhotoResponse = await singleAPICall(DOG_IMG_API);
+    const dogPhotoData = await dogPhotoResponse.json();
+    const dogPhotoURL = dogPhotoData.message;
+    replaceIMG(dogPhotoURL, "#api0-container", "Random Dog Photo");
 });
+
+catAPIButton.addEventListener("click", async () => {
+    const catPhotoResponse = await singleAPICall(CAT_IMG_API);
+    const catPhotoData = await catPhotoResponse.json();
+    const catPhotoURL = catPhotoData[0].url;
+    replaceIMG(catPhotoURL, "#api1-container", "Random Cat Photo");
+});
+
+//
