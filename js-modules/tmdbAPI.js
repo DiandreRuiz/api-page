8dfb770ad93a8c8cdcbf91bd439e3ce87ffcb415
DiandreRuiz <Diandreruiz77@gmail.com>
2025-08-27 14:12:58 -0400

    Finished part 1 of getTMDResopnse function, getting results including movie IDs for movies now playing in theatres

--- js-modules/tmdbAPI.js
+++ js-modules/tmdbAPI.js
@@ -1,7 +1,7 @@
 import { singleAPICall } from "./apiClient.js";
 import { errorDisplay } from "./uiUtils.js";
 
-const TMDB_ENDPOINT = "https://api.themoviedb.org/3/configuration";
+const TMDB_ENDPOINT = "https://api.themoviedb.org/3/movie/now_playing";
 
 // Load in API creds
 const getAPIKey = async () => {
@@ -10,13 +10,22 @@ const getAPIKey = async () => {
     return data.TMDB_API_KEY;
 };
 
-// Function that returns result from api call
+// Function that returns the names of movies currently in theatres
 export const getTMDBResponse = async () => {
+    // Get movie ID's
     const apiKey = await getAPIKey();
     const response = await singleAPICall(TMDB_ENDPOINT, {
         Authorization: `Bearer ${apiKey}`,
         accept: "application/json",
     });
+    const data = await response.json();
+    const nowPlayingResults = data.results;
+    if (!nowPlayingResults.length > 0) {
+        throw new Error("Could not retrieve results from TMDB API");
+    }
+
+    // Look up movie ID's
+    const movieNames = [];
 };
 
 // Function that sets up event handler for the button and is exported
